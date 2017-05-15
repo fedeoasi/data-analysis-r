e4b27525a540f8db4314dc1e69f6c190830360cd
Federico Caimi <fedeoasi@gmail.com>
2017-05-14 21:00:21 -0500

    Initial work on CTA ridership. Move jekyll script to root so that it publishes multiple projects.

---
+++ jekyll.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+BLOG_DIR=blog
+cp jodi/jodi-oil-data.md $BLOG_DIR/_posts/2017-05-07-jodi-oil-data.md
+cp cta/cta.md $BLOG_DIR/_posts/2017-05-13-cta.md
+ASSETS_DIR=$BLOG_DIR/assets/
+cp -R jodi/assets/* $ASSETS_DIR
+cp -R cta/assets/* $ASSETS_DIR
