477be0bb594e564e7cf7395b58cf9a82c3e4c49b
Federico Caimi <fedeoasi@gmail.com>
2017-05-21 09:58:24 -0500

    Lots of improvements on the CTA blog post and update date in the Jekyll script

--- jekyll.sh
+++ jekyll.sh
@@ -2,7 +2,7 @@
 
 BLOG_DIR=blog
 cp jodi/jodi-oil-data.md $BLOG_DIR/_posts/2017-05-07-jodi-oil-data.md
-cp cta/cta.md $BLOG_DIR/_posts/2017-05-13-cta.md
+cp cta/cta.md $BLOG_DIR/_posts/2017-05-21-cta.md
 ASSETS_DIR=$BLOG_DIR/assets/
 cp -R jodi/assets/* $ASSETS_DIR
 cp -R cta/assets/* $ASSETS_DIR
