7634795186e28af2a150127fedb787ac32f8d8bd
Federico Caimi <federico.caimi@here.com>
2017-05-24 11:44:42 -0500

    Do not redownload an F1 quali file if it's already on disk

--- f1-quali/download_datasets.sh
+++ f1-quali/download_datasets.sh
@@ -1,8 +1,11 @@
 mkdir -p data
 
 function download_data(){
-  sleep 0.2
-  wget -O "data/$1_$2.json" http://ergast.com/api/f1/$2/drivers/$1/qualifying.json
+  FILE=data/$1_$2.json
+  if [ ! -f $FILE ]; then
+    sleep 0.2
+    wget -O $FILE http://ergast.com/api/f1/$2/drivers/$1/qualifying.json
+  fi
 }
 
 download_data 'alonso' '2016'
@@ -19,3 +22,7 @@ download_data 'vettel' '2017'
 
 download_data 'alonso' '2007'
 download_data 'hamilton' '2007'
+
+download_data 'alonso' '2010'
+download_data 'vettel' '2010'
+
