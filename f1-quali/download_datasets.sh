479bbfd77edaca8964402eff08b48e9f17cae303
Federico Caimi <federico.caimi@here.com>
2017-05-24 09:00:52 -0500

    Clean up F1 qualy download data script

--- f1-quali/download_datasets.sh
+++ f1-quali/download_datasets.sh
@@ -5,13 +5,17 @@ function download_data(){
   wget -O "data/$1_$2.json" http://ergast.com/api/f1/$2/drivers/$1/qualifying.json
 }
 
-#wget -O "data/vettel_2017.json" http://ergast.com/api/f1/2017/drivers/vettel/qualifying.json
-#wget -O "data/hamilton_2017.json" http://ergast.com/api/f1/2017/drivers/hamilton/qualifying.json
-#wget -O "data/hamilton_2016.json" http://ergast.com/api/f1/2016/drivers/hamilton/qualifying.json
-#wget -O "data/rosberg_2016.json" http://ergast.com/api/f1/2016/drivers/rosberg/qualifying.json
-#wget -O "data/hamilton_2007.json" http://ergast.com/api/f1/2007/drivers/hamilton/qualifying.json
-#wget -O "data/alonso_2007.json" http://ergast.com/api/f1/2007/drivers/alonso/qualifying.json
-
 download_data 'alonso' '2016'
 download_data 'button' '2016'
+
 download_data 'hamilton' '2016'
+download_data 'rosberg' '2016'
+
+download_data 'vettel' '2016'
+download_data 'raikkonen' '2016'
+
+download_data 'hamilton' '2017'
+download_data 'vettel' '2017'
+
+download_data 'alonso' '2007'
+download_data 'hamilton' '2007'
