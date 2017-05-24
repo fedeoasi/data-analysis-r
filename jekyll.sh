5fe4fa953ffa71c53a3ecd594438b8dd500574f7
Federico Caimi <federico.caimi@here.com>
2017-05-24 08:59:52 -0500

    Add f1-qualy subfolder to jekyll generation script

--- jekyll.sh
+++ jekyll.sh
@@ -3,6 +3,8 @@
 BLOG_DIR=blog
 cp jodi/jodi-oil-data.md $BLOG_DIR/_posts/2017-05-07-jodi-oil-data.md
 cp cta/cta.md $BLOG_DIR/_posts/2017-05-21-cta.md
+cp f1-quali/quali.md $BLOG_DIR/_posts/2017-05-24-f1-quali.md
 ASSETS_DIR=$BLOG_DIR/assets/
 cp -R jodi/assets/* $ASSETS_DIR
 cp -R cta/assets/* $ASSETS_DIR
+cp -R f1-quali/assets/* $ASSETS_DIR
